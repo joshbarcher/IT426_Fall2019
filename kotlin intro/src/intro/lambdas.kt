f9d0dbc7539d169b30383823d02ef47d03d85e89
Josh Archer <jarcher@greenriver.edu>
2019-10-07 00:20:54 -0700

    Add a few extra examples of anonymous functions in Kotlin.

--- kotlin intro/src/intro/lambdas.kt
+++ kotlin intro/src/intro/lambdas.kt
@@ -9,10 +9,31 @@ fun main()
 
     println(calculator(adder, 10, 20))
     println(calculator(multiplier, 30, 10))
+
+    val transforms = arrayOf<(String) -> String>(
+        { it.replace(" ", "_") },
+        { it.toLowerCase() },
+        { it.replace("a", "@") }
+    )
+    val inputString = "I know you can be over and under whelmed, " +
+                      "can you ever just be whelmed"
+    val interestingString = applyStringTransforms(transforms, inputString)
+    println("After changes - $interestingString")
 }
 
 fun calculator(func: (Int, Int) -> Int, op1: Int, op2: Int): Int
 {
     return func(op1, op2)
+}
+
+fun applyStringTransforms(transforms: Array<(String) -> String>, input: String) : String
+{
+    var changes = input
+    for (transform in transforms)
+    {
+        changes = transform(changes)
+    }
+
+    return changes
 }
 
