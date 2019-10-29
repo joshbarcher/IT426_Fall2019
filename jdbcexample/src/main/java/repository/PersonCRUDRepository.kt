191b6536c1017d47c73340d2ee667a91a25e7d5d
Josh Archer <jarcher@greenriver.edu>
2019-10-29 12:50:04 -0700

    Added an example of database connectivity and CRUD methods for a JDBC connection to a mysql database.

--- jdbcexample/src/main/java/repository/PersonCRUDRepository.kt
+++ jdbcexample/src/main/java/repository/PersonCRUDRepository.kt
@@ -1,6 +1,7 @@
 package repository
 
 import entities.Person
+import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.Statement
 
@@ -30,22 +31,66 @@ class PersonCRUDRepository : DBConnection()
 
     fun findById(id: Int): Person?
     {
+        val pStmt: PreparedStatement = connection.prepareStatement(
+        "SELECT id, fName, lName, nickname, age FROM people WHERE id = ?"
+        )
+
+        pStmt.setInt(1, id)
+        val results: ResultSet = pStmt.executeQuery()
+
+        if (results.next())
+        {
+            return Person(
+                id = results.getInt("id"),
+                fName = results.getString("fName"),
+                lName = results.getString("lName"),
+                nickname =  results.getString("nickname"),
+                age = results.getInt("age")
+            )
+        }
         return null
     }
 
     fun save(person: Person)
     {
+        val pStmt: PreparedStatement = connection.prepareStatement(
+        "INSERT INTO people (fName, lName, nickname, age) VALUES (?, ?, ?, ?)",
+            Statement.RETURN_GENERATED_KEYS
+        )
 
+        //bind my parameters to the query (avoid SQL injection)
+        pStmt.setString(1, person.fName)
+        pStmt.setString(2, person.lName)
+        pStmt.setString(3, person.nickname)
+        pStmt.setInt(4, person.age)
+
+        person.id = pStmt.executeUpdate()
     }
 
     fun update(person: Person)
     {
+        val pStmt: PreparedStatement = connection.prepareStatement(
+        "UPDATE people SET fName = ?, lName = ?, nickname = ?, age = ? " +
+            "WHERE id = ?"
+        )
 
+        pStmt.setString(1, person.fName)
+        pStmt.setString(2, person.lName)
+        pStmt.setString(3, person.nickname)
+        pStmt.setInt(4, person.age)
+        pStmt.setInt(5, person.id)
+
+        pStmt.execute()
     }
 
     fun delete(id: Int)
     {
+        val pStmt: PreparedStatement = connection.prepareStatement(
+        "DELETE FROM people WHERE id = ?"
+        )
 
+        pStmt.setInt(1, id)
+        pStmt.execute()
     }
 }
 
--- jdbcexample/src/main/java/test/testing.kt
+++ jdbcexample/src/main/java/test/testing.kt
@@ -1,12 +1,42 @@
 package test
 
+import entities.Person
 import repository.PersonCRUDRepository
 
 fun main()
 {
     val repo = PersonCRUDRepository()
-    val people = repo.findAll()
 
+    //enter a few test records
+    /*repo.save(Person(
+        id = 0,
+        fName = "William",
+        lName = "Perry",
+        nickname = "The Fridge",
+        age = 45
+    ))
+
+    repo.save(Person(
+        id = 0,
+        fName = "Massie",
+        lName = "Williams",
+        nickname = "Arya",
+        age = 24
+    ))*/
+
+    //update the first record
+    val person1 = repo.findById(1)
+    if (person1 != null)
+    {
+        person1.fName = "Juan"
+
+        repo.update(person1);
+    }
+
+    //delete the second record
+    repo.delete(2)
+
+    val people = repo.findAll()
     for (person in people)
     {
         println(person)
