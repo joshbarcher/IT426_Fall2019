c397700a3ec4bb1b778f9de74528d5647ad692ae
Josh Archer <jarcher@greenriver.edu>
2019-10-31 11:55:03 -0700

    Added a JPA repository to our application.

---
+++ spring-first-project/src/main/kotlin/edu/greenriver/it/springfirstproject/repositories/IMessageRepository.kt
@@ -0,0 +1,11 @@
+package edu.greenriver.it.springfirstproject.repositories
+
+import edu.greenriver.it.springfirstproject.model.Message
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface IMessageRepository : CrudRepository<Message, Long>
+{
+
+}
