90aa9375734805304460a4dc0a508519fb662a58
Josh Archer <jarcher@greenriver.edu>
2019-11-01 11:22:30 -0700

    Added an example of the template method pattern.

---
+++ template-method-pattern/src/template/driver.kt
@@ -0,0 +1,12 @@
+package template
+
+fun main()
+{
+    val thriller = ThrillerMovie()
+    val action = ActionMovie()
+
+    thriller.playMovie("The Shark", "Hopper")
+    println()
+    action.playMovie("Thulsa Doom", "Conan")
+    println()
+}
